40dcb04153d0a36f0fccd6ca1d406955f491c84b
rosua328 <rosua328@gmail.com>
2020-06-06 13:48:21 +0900

    저장 위치 수정, 로그인 창 UI 개선

--- mainpro/src/mainpro/Main.java
+++ mainpro/src/mainpro/Main.java
@@ -6,7 +6,10 @@ public class Main {
 	
 	public static void main(String[] args) 
 	{
-		new Login();
+		//new Login();
 		//new Sign();
+		//new SubList();
+		//new ConditionIN();
+		new Outcome();
 	}
 }
