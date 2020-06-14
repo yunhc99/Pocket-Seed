f00c1928420dbaf2a2f71122bea08aa40a0af1c2
rosua328 <rosua328@gmail.com>
2020-06-15 04:25:09 +0900

    PULL에 대한 수정내용 수정

--- mainpro/src/mainpro/Main.java
+++ mainpro/src/mainpro/Main.java
@@ -1,9 +1,15 @@
 package mainpro;
 
 public class Main {
+	//GUI 크기 설정 함수입니다.
 	public static final int SCREEN_WIDTH = 600;
 	public static final int SCREEN_HEIGHT = 900;
 
+
+	//데이터 베이스 관련 입력 함수입니다 맞게 변경 해주세요
+	public static final String url = "jdbc:mysql://localhost/seed?useSSL=false";
+	public static final String id = "root";
+	public static final String pw = "0000";
 	public static void main(String[] args)
 	{
 		//new Login();
